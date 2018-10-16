6b1de6420d07ff73d76d18f7b0c8d3ba5459462d
Ramasubramanian Sankar <ram@ajirasoft.com>
2018-10-16 17:39:36 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # jmh-benchmarks
+
+### Instructions to run
+1. Go to project root and run
+` mvn clean install`
+2. Then run 
+`java -jar target/benchmarks.jar`
+
+Sample run results on my Macbook Pro Mid 2014:
+
+```
+# Run complete. Total time: 00:16:45
+
+REMEMBER: The numbers below are just data. To gain reusable insights, you need to follow up on
+why the numbers are the way they are. Use profilers (see -prof, -lprof), design factorial
+experiments, perform baseline and negative tests that provide experimental control, make sure
+the benchmarking environment is safe on JVM/OS/HW level, ask for reviews from the domain experts.
+Do not assume the numbers tell you what you want them to tell.
+
+Benchmark                     Mode  Cnt   Score    Error  Units
+MyBenchmark.testMethod        avgt   25  ≈ 10⁻⁸            s/op
+MyBenchmark.testMethodStatic  avgt   25  ≈ 10⁻⁸            s/op
+```
+
+**Inference**
+
+Both executing non static and static does not have any performance improvements even at the scale of 10 ^ -8. So adding static is just a matter of convention than for performance.
---
+++ src/main/java/in/raam/MyBenchmark.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package in.raam;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyBenchmark {
+
+    static class MyBean {
+        private Integer id;
+        private String name;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    private MyBean toMyBean(Integer id, String name) {
+        MyBean myBean = new MyBean();
+        myBean.setId(id);
+        myBean.setName(name);
+        return myBean;
+    }
+
+    private static MyBean toMyBeanStatic(Integer id, String name) {
+        MyBean myBean = new MyBean();
+        myBean.setId(id);
+        myBean.setName(name);
+        return myBean;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public void testMethod() {
+        List<MyBean> myBeans = new ArrayList<>();
+        myBeans.add(toMyBean(1, "Ram"));
+        myBeans.add(toMyBean(2,"Adarsh"));
+        myBeans.add(toMyBean(3, "Prabhu"));
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public void testMethodStatic() {
+        List<MyBean> myBeans = new ArrayList<>();
+        myBeans.add(toMyBeanStatic(1, "Ram"));
+        myBeans.add(toMyBeanStatic(2,"Adarsh"));
+        myBeans.add(toMyBeanStatic(3, "Prabhu"));
+    }
+}
